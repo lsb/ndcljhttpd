900639ec985fd7891b8c0362b07cf5db95f32179
lsb <leebutterman@gmail.com>
2016-11-26 14:21:52 -0800

    Make the front page; wip to be put elsewhere

--- src/clj/ndcljhttpd/routes/home.clj
+++ src/clj/ndcljhttpd/routes/home.clj
@@ -2,11 +2,27 @@
   (:require [ndcljhttpd.layout :as layout]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
+            [ndcljhttpd.db.core :as db]
+            [nd.htmlconversions]
             [clojure.java.io :as io]))
 
 (defn home-page []
   (layout/render
     "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+
+(defn default-session []
+  {:id (System/currentTimeMillis)
+   :slider 3
+   :pparts true
+   :notes true
+   :language "english"})
+
+(defn get-session-lol [] {})
+
+(defn odi-et-amo [] (time (nd.htmlconversions/subpassageid-to-html db/*db* (:id (first (db/first-subpassage-of-passage-in-book {:bookauthor "Juvenal" :booktitle "Satires" :passagetitle "6"}))))))
+
+(defn front-page []
+  (layout/make-a-page {:text (odi-et-amo)} (merge (default-session) (get-session-lol)) "/"))
 
 (defn about-page []
   (layout/render "about.html"))
