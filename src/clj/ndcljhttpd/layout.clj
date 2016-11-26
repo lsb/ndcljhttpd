8f6a0414b850c230e975a74875af282736995707
lsb <leebutterman@gmail.com>
2016-11-26 14:00:29 -0800

    Make a page, + 2 misplaced helpers

--- src/clj/ndcljhttpd/layout.clj
+++ src/clj/ndcljhttpd/layout.clj
@@ -25,6 +25,46 @@
           :servlet-context *app-context*)))
     "text/html; charset=utf-8"))
 
+(defn logged-in [session] (< (:id session) 100000000))
+
+(defn user-link [u] (str "/people/" (:urlname u)))
+
+(defn make-a-page
+  "render page.html.selmer with sensible defaults"
+  [options session request-uri]
+  (let [salutation
+          (if (not (logged-in session))
+             (str "Hi there. <a rel='nofollow' href='" (hiccup.util/url "/login" {:from request-uri}) "'>Login or signup free.</a>")
+             (let [username (:name (:user session))
+                   upcase (not= (clojure.string/lower-case username) username)]
+               (str (if upcase "Hello, " "hi ")
+                    " "
+                    username
+                    ". <a href='"
+                    (hiccup.util/url (user-link (:user session)))
+                    "'>"
+                    (if upcase "V" "v")
+                    "iew profile</a> &middot; <a href='"
+                    (hiccup.util/url "/logout" {:from request-uri}))))]
+    (render "page.html.selmer"
+      (merge
+        {
+          :author "Author"
+          :title "Title"
+          :javascript ""
+          :window_title (str (:author options) " " (:title options) ", with adjustable running vocabulary under each word.")
+          :page_description (str "Read " (:author options) " " (:title options) ", with adjustable running vocabulary under each word.")
+          :text "Text"
+          :no_text_page true
+          :slider_value (:slider session)
+          :principal_parts (:pparts session)
+          :text_notes (:notes session)
+          :login_string salutation
+          :language (:language session)
+          :blue_box true
+        }
+        options))))
+
 (defn error-page
   "error-details should be a map containing the following keys:
    :status - error status
